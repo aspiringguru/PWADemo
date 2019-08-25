7ce653088b348cc0a0c58ea8bb817a24ac9fb123
aspiringguru <bmatthewtaylor@gmail.com>
2019-08-25 18:33:49 +1000

    https removed from server

---  PWADemo_server.js
+++  PWADemo_server.js
@@ -20,7 +20,8 @@
 
 const express = require('express');
 const fetch = require('node-fetch');
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+//remove redirectToHTTPS since aws config uses nginx  
+//const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
 // CODELAB: Change this to add a delay (ms) before the server responds.
 const FORECAST_DELAY = 0;
@@ -171,7 +172,7 @@ function startServer() {
   const app = express();
 
   // Redirect HTTP to HTTPS,
-  app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+  //app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 
   // Logging for each request
   app.use((req, resp, next) => {
